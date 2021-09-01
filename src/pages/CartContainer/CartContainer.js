d35bb546d88d94442e0eb077d597f281e874a911
Toto Argumedo <totoargumedo@gmail.com>
2021-09-01 00:22:12 -0300

    correcion tamaño imagen /cart

--- src/pages/CartContainer/CartContainer.js
+++ src/pages/CartContainer/CartContainer.js
@@ -9,7 +9,7 @@ import meme from "../../assets/meme1.jpg";
 const CartContainer = () => {
   return (
     <div>
-      <img src={meme} alt="No hay cart" />
+      <img src={meme} alt="No hay cart" className="meme" />
     </div>
   );
 };
