656d446b749354b4caf76b2012b6a9697c6d628b
Toto Argumedo <totoargumedo@gmail.com>
2021-09-02 21:49:44 -0300

    Se coloco contador en CartWidget.js en funcion al lenght de cart

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 
 // STYLING
 import "./CartWidget.css";
 
 // LIBRERIAS
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+
+import { Icon } from "semantic-ui-react";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCartArrowDown } from "@fortawesome/free-solid-svg-icons";
+// PROVIDER
+import { CartContext } from "../../context/CartContext";
+import ItemSearch from "../ItemSearch/ItemSearch";
 
 export const CartWidget = () => {
+  const [cart, setCart] = useContext(CartContext);
+
   return (
     <div className="headerWidget">
-      <FontAwesomeIcon icon={faCartArrowDown} size="2x" />;
+      {/* <FontAwesomeIcon icon={faCartArrowDown} size="2x" />; */}
+      <Icon name="cart" size="large" />
+      <p>{ItemSearch.length}</p>
     </div>
   );
 };
